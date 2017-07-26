972f59a1445fa296594b2333e0a82b4d507d2154
Geore Tada <tada@people.co.jp>
2017-07-26 15:06:33 +0900

    CMYK の総数が 300% 以上のリッチブラックのチェックを追加した

--- checker.jsx
+++ checker.jsx
@@ -1,7 +1,8 @@
 //
 // 入稿チェックスクリプト
-// 2017/07/14
+// 2017/07/26
 //
+
 
 #include "./checker/colorModeCheck.jsx"
 #include "./checker/fontCheck.jsx"
@@ -13,10 +14,12 @@
 #include "./checker/lockedItemCheck.jsx"
 #include "./checker/guideItemCheck.jsx"
 #include "./checker/isolatedItemCheck.jsx"
+#include "./checker/cmykCheck.jsx"
 
 
 var win = new Window("dialog", "checker")
 displayDialog(win, [10, 10, 370, 70], "カラーモードのチェック", colorModeCheck())
+displayDialog(win, [10, 10, 370, 70], "CMYK総数300%チェック", cmykCheck())
 displayDialog(win, [10, 10, 370, 70], "フォントデータのチェック", fontCheck())
 displayDialog(win, [10, 10, 370, 70], "特色オブジェクトのチェック", spotColorCheck())
 displayDialog(win, [10, 10, 370, 70], "リンク画像のチェック", linkImageCheck())
---
+++ checker/cmykCheck.jsx
@@ -0,0 +1,44 @@
+// CMYK の総数が 300% 以上であるのかチェックする( プリントネットの入稿規定 )
+//
+// return  { result: Boolean (true is success)
+//           message: "hello..." (some text) }
+
+function cmykCheck() {
+  var doc = app.activeDocument
+  var result = false
+  var message = []
+  var checkColor = new CMYKColor()
+  checkColor.cyan = 0
+  checkColor.magenta = 100
+  checkColor.yellow = 100
+  checkColor.black = 0
+
+  for (i = 0; i < doc.pathItems.length; i++) {
+    color = doc.pathItems[i].fillColor
+    layer = doc.pathItems[i].layer
+
+    sum = color.cyan + color.magenta + color.yellow + color.black
+
+    if (sum >= 300 && !layer.locked) {
+      doc.pathItems[i].strokeColor = checkColor
+      doc.pathItems[i].strokeWidth = 5
+
+      message.push(
+        "C: " +
+          color.cyan +
+          " M: " +
+          color.magenta +
+          " Y: " +
+          color.yellow +
+          " K: " +
+          color.black,
+      )
+    }
+  }
+
+  if (message.length == 0) {
+    message[0] = "CMYKの総数300%以下です"
+    result = true
+  }
+  return {result: result, message: message}
+}
